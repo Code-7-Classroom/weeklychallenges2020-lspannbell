0437f85a1871aebbebbfdb34f4cf5d0d95eba5dc
lspannbell <lmspannbell@gmail.com>
2020-03-30 11:22:22 -0400

    commit

---
+++ Checkpoints/CheckpointArrays/CheckpointArrays.js
@@ -0,0 +1,13 @@
+// drinks: water coffee tea
+// foods: apple sandwich chips
+// meals: combine both 
+// reverse Array
+
+var drinks = 'water, coffee, tea';
+console.log(drinks);
+
+var foods = 'apple, sandwich, chips';
+console.log(foods);
+
+console.log(drinks, foods);
+console.log(foods, drinks)
