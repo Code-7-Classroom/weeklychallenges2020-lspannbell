d9d7ffee75b9ebe9957d4b9fcfec8f3eb6de4617
lspannbell <lmspannbell@gmail.com>
2020-04-13 17:06:47 -0400

    commit

---
+++ Checkpoints/ES6/ES6.js
@@ -0,0 +1,7 @@
+
+let me = (name) => {
+    name = "Lenise Bell"
+}
+
+/* I really thought I had an understanding of the material today. clearly I do not. 
+I will definitely be in office hours for the remainder of the week.*/
