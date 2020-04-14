4dd78d0fae5bfadfe7e70f00b2d3221b943445a5
lspannbell <lmspannbell@gmail.com>
2020-04-13 21:21:29 -0400

    commit

---
+++ JavascriptWeek4/medium/medum.js
@@ -0,0 +1,6 @@
+var array = [4, 5, 6, 7, 0, 1, 2];
+
+console.log(array.indexOf(4));
+console.log(array.indexOf(3));
+//searching for "4" returns index of 0. searching for "3"
+//returns -1 because it does not exist in the array
